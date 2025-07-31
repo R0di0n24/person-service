3780ea30588e7015d57828f048aadc0acac83dd3
Rodion <rodion1900@gmail.com>
2025-07-31 15:33:18 +0300

    person-service inheritance homework

--- src/main/java/telran/java58/person/dao/PersonRepository.java
+++ src/main/java/telran/java58/person/dao/PersonRepository.java
@@ -1,16 +1,11 @@
 package telran.java58.person.dao;
 
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import telran.java58.person.dto.CityPopulationDto;
-import telran.java58.person.dto.PersonDto;
-import telran.java58.person.model.Address;
 import telran.java58.person.model.Child;
 import telran.java58.person.model.Employee;
 import telran.java58.person.model.Person;
-
 import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Stream;
@@ -22,34 +17,12 @@ public interface PersonRepository extends JpaRepository<Person, Integer> { //gen
     Person[] findArrayByNameIgnoreCase(String name);
 
     Stream<Person> findStreamByAddressCityIgnoreCase(String city);
+
     @Query("select p from Person p where p.address.city=?1")
     Person[] findArrayByAddressCity(String city);
 
     Person[] findArrayByBirthDateBetween(LocalDate from, LocalDate to);
 
-
-
-//    List<Person> findByNameIgnoreCase(String name);
-//    List<Person> findByAddress_City(String city);
-
-//    @Query("SELECT p FROM Person p WHERE FUNCTION('TIMESTAMPDIFF', 'YEAR', p.birthDate, CURRENT_DATE) BETWEEN :minAge AND :maxAge")
-//    List<Person> findPersonsBetweenAges(@Param("minAge") int minAge, @Param("maxAge") int maxAge);
-
-//@Query("SELECT new telran.java58.person.dto.CityPopulationDto(p.address.city, COUNT(p)) " +
-//        "FROM Person p WHERE p.address.city IS NOT NULL " +
-//        "GROUP BY p.address.city ORDER BY p.address.city")
-
-//TODO
-    // Select city as CITY, count(city) as POPULATION from persons Group By city;
-
-    //@Query("SELECT new telran.java58.person.dto.CityPopulationDto('city', COUNT(p)) " +
-//        "FROM Person p JOIN p.address a " +
-//        "WHERE 'city' IS NOT NULL " +
-//        "GROUP BY 'city' " +
-//        "ORDER BY 'city' ")
-//@Query("Select 'city' as CITY, count(city) as POPULATION from 'persons' Group By city")
-//    @Query(value = "SELECT city AS city, COUNT(city) AS population FROM persons GROUP BY city",
-//            nativeQuery = true)
     @Query("select new telran.java58.person.dto.CityPopulationDto(p.address.city, count (p)) from Person p group by p.address.city order by count(p)")
     List<CityPopulationDto> getCitiesPopulation();
 
